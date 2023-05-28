394624144cba1b659c78f2daf59321aa7441a3d4
Mike <mikhail.yakhyayeu@gmail.com>
2023-05-28 18:00:54 +0300

    Close button

---
+++ tool/src/molecules/CloseButton.js
@@ -0,0 +1,7 @@
+import styles from './CloseButton.module.scss'
+
+const CloseButton = () => {
+    return (<button class={styles.close}></button>);
+}
+
+export default CloseButton;
--- tool/src/molecules/TodoItem.js
+++ tool/src/molecules/TodoItem.js
@@ -1,10 +1,15 @@
 import style from "./TodoItem.module.scss";
 
 import Checkbox from "../atoms/Checkbox"
+import CloseButton from "./CloseButton";
 
 const TodoItem = () => {
     return (
-        <li className={style.container}><Checkbox /><span>{"Some very important task"}</span><button>X</button></li>
+        <li className={style.container}>
+                <Checkbox />
+                <span>{"Some very important task"}</span>
+                <CloseButton />
+        </li>
     )
 }
 
