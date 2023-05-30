ae4a195c9fd31c4ed17346a0172aabc4226685e2
Mike <mikhail.yakhyayeu@gmail.com>
2023-05-30 15:30:15 +0300

    saving to local storage

--- tool/src/store/index.js
+++ tool/src/store/index.js
@@ -1,10 +1,20 @@
 import {configureStore} from "@reduxjs/toolkit";
 import todoListSlice from "./todo-list-slice";
 
+var persistedState = JSON.parse(localStorage.getItem("state"));
+
 const store = configureStore({
     reducer: {
         todoList: todoListSlice.reducer,
     },
+    preloadedState: persistedState === null ? {} : persistedState
+});
+
+store.subscribe(() => {
+    var state = store.getState();
+
+    var jsonState = JSON.stringify(state);
+    localStorage.setItem("state", jsonState);
 });
 
 export default store;
